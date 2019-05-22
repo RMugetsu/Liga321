bac2cb811e1d096f6183a3f77c0ad335b0b53f8e
RMugetsu <rgarciapaules@iesesteveterradas.cat>
2019-05-22 16:28:14 +0200

    comprobar fechas partido

--- resources/views/partido.blade.php
+++ resources/views/partido.blade.php
@@ -4,14 +4,15 @@
     <div class="row">
         <div class="col-md-12 tituloPartido">
         </div>
-        <div class="col-md-12">
+        <div class="col-md-12 contenido">
+
         </div>
     </div>
 </div>
 <script>
     var partido = {!! json_encode($partido->toArray(), JSON_HEX_TAG) !!}[0] ;
     var equipos = {!! json_encode($equipos->toArray(), JSON_HEX_TAG) !!} ;
-    console.log(partido);
+    console.log(equipos);
 
     var div_titulo = $(".tituloPartido");
     var gollocal = 0;
@@ -21,11 +22,11 @@
     var equipos_partido = equipo1 +" "+gollocal+ " - "+golvisitante+" " + equipo2 ;
     div_titulo.append($("<h2>").text(equipos_partido));
 
-    var fecha =  partido['fechainicio'];
+    var fecha =  partido['fechainicio'].split("-");
     var hora =  partido['horadeinicio'];
-    var moment  =  new moment();
-    var fechaActual = moment.format("Y-M-D");
-    var horaActual = parseInt(moment.format("H"),10);
+    var momento = new moment();
+    var fechaActual = momento.format("L").split("/");
+    var horaActual = parseInt(momento.format("H"),10);
     var nose = moment("2019-05-22").isSame(fechaActual);
     console.log(nose);
     // if(moment(fecha).isSame(fechaActual)){
@@ -33,16 +34,19 @@
     // }else{
     //     console.log("no funciona");
     // };
+    
     console.log(horaActual);
     function comprobarDiaDelPartido(fecha,fechaActual){
-        if(parseInt(fecha[0],10)===parseInt(fechaActual[0],10)){
-            if(fecha[1]==fechaActual[1]){
-                if("21"==fechaActual[1]){
+        console.log(fecha,fechaActual);
+        var prueba = "22";
+        if(fecha[0]==fechaActual[2]){
+            if(fecha[1]==fechaActual[0]){
+            console.log(prueba,fecha[2]);
+            console.log(typeof(prueba),typeof(fecha[1]));
+                if(prueba===fecha[2]){
                     console.log("se juega el partido hoy");
                     return true;
                 }
-                console.log(fecha[1],fechaActual[1]);
-                console.log("1");
                 return false;
             }
             console.log(fecha[0],fechaActual[0]);
@@ -52,14 +56,26 @@
     }
     function comprobarHorarioDelPartido(fecha,fechaActual,hora,horaActual){
         if (comprobarDiaDelPartido(fecha,fechaActual)){
-            if(hora>horaActual){
-                console.log("El partido aun no se ha jugado")
+            console.log(hora, horaActual);
+            if(parseInt(hora,10)>horaActual){
+                   mensajeDePartidoSinJugar(fecha,hora);
+            }
+            else if( parseInt(hora,10)==horaActual || parseInt(hora,10)+1==horaActual ){
+                jugadoresDeLosEquiposDelPartido();
+                mensajeDePartidoSinJugar(fecha,hora);
+            }
+            else if(parseInt(hora,10)<horaActual){
+                console.log("El partido ya se ha jugado");
             }
         }
     }
 
     comprobarHorarioDelPartido(fecha,fechaActual,hora,horaActual);
     
+    function mensajeDePartidoSinJugar(fecha,hora){
+        var mensaje = "El partido se jugará el " +fecha[2]+" de "+fecha[1]+" del "+fecha[0]+ " a las "+ hora+":00H";
+        $(".contenido").append($("<h2>").text(mensaje));
+    }
 
     var jugadores_local = traerDatosJugadores(partido['equipolocal']);
     var jugadores_visitante = traerDatosJugadores(partido['equipovisitante']);
